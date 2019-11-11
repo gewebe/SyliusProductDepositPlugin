6c85a63de781710a578e83925b0d897eab60487c
Gerd Weitenberg <gweitenb@gmail.com>
2019-11-11 21:57:31 +0100

    first specs for entities added

---
+++ spec/Entity/ChannelDepositSpec.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Gweb\SyliusProductDepositPlugin\Entity;
+
+use Gweb\SyliusProductDepositPlugin\Entity\ChannelDeposit;
+use Gweb\SyliusProductDepositPlugin\Entity\ChannelDepositInterface;
+use Gweb\SyliusProductDepositPlugin\Entity\ProductVariantInterface;
+use PhpSpec\ObjectBehavior;
+
+final class ChannelDepositSpec extends ObjectBehavior
+{
+    function it_is_channel_deposit(): void
+    {
+        $this->shouldHaveType(ChannelDeposit::class);
+    }
+
+    function it_implements_channel_deposit_interface(): void
+    {
+        $this->shouldImplement(ChannelDepositInterface::class);
+    }
+
+    function it_has_price(): void
+    {
+        $this->getPrice()->shouldReturn(null);
+        $this->setPrice(null);
+        $this->setPrice(25);
+        $this->getPrice()->shouldReturn(25);
+    }
+
+    function it_has_product_variant(ProductVariantInterface $productVariant): void
+    {
+        $this->getProductVariant()->shouldReturn(null);
+        $this->setProductVariant(null);
+        $this->setProductVariant($productVariant);
+        $this->getProductVariant()->shouldReturn($productVariant);
+    }
+
+    function is_has_channel_code(): void
+    {
+        $this->getChannelCode()->shouldReturn(null);
+        $this->setChannelCode(null);
+        $this->setChannelCode('de');
+        $this->getChannelCode()->shouldReturn('de');
+    }
+}
---
+++ spec/Entity/ProductVariantSpec.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Gweb\SyliusProductDepositPlugin\Entity;
+
+use Doctrine\Common\Collections\Collection;
+use Gweb\SyliusProductDepositPlugin\Entity\ChannelDepositInterface;
+
+use Gweb\SyliusProductDepositPlugin\Entity\ProductVariant;
+use Gweb\SyliusProductDepositPlugin\Entity\ProductVariantInterface;
+use PhpSpec\ObjectBehavior;
+use Sylius\Component\Core\Model\ChannelInterface;
+use Sylius\Component\Taxation\Model\TaxCategoryInterface;
+
+final class ProductVariantSpec extends ObjectBehavior
+{
+    function it_is_sylius_product_variant(): void
+    {
+        $this->shouldHaveType(ProductVariant::class);
+    }
+
+    function it_implements_product_variant_interface(): void
+    {
+        $this->shouldImplement(ProductVariantInterface::class);
+    }
+
+    function it_has_no_default_channel_deposits(): void
+    {
+        $this->getChannelDeposits()->shouldReturnAnInstanceOf(Collection::class);
+        $this->getChannelDeposits()->shouldHaveCount(0);
+    }
+
+    function it_has_channel_deposit_for_channel(ChannelInterface $channel, ChannelDepositInterface $channelDeposit): void
+    {
+        $this->getChannelDepositForChannel($channel)->shouldReturn(null);
+
+        $this->addChannelDeposit($channelDeposit);
+
+        $this->hasChannelDeposit($channelDeposit)->shouldReturn(true);
+        $this->hasChannelDepositForChannel($channel)->shouldReturn(true);
+
+        $this->getChannelDepositForChannel($channel)->shouldReturn($channelDeposit);
+
+        $this->removeChannelDeposit($channelDeposit);
+
+        $this->hasChannelDeposit($channelDeposit)->shouldReturn(false);
+        $this->hasChannelDepositForChannel($channel)->shouldReturn(false);
+    }
+
+    function it_has_tax_category(TaxCategoryInterface $taxCategory): void
+    {
+        $this->getDepositTaxCategory()->shouldReturn(null);
+        $this->setDepositTaxCategory(null);
+
+        $this->setDepositTaxCategory($taxCategory);
+        $this->getDepositTaxCategory()->shouldReturn($taxCategory);
+    }
+}
