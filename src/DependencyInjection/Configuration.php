e5248e4a0e822f7538e4b80d60b34174ffd794ec
Gerd Weitenberg <gweitenb@gmail.com>
2019-11-11 17:55:28 +0100

    get root node with new function from TreeBuilder

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -14,8 +14,13 @@ final class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('gweb_deposit');
+        $treeBuilder = new TreeBuilder('gweb_deposit');
+        if (\method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('gweb_deposit');
+        }
 
         return $treeBuilder;
     }
