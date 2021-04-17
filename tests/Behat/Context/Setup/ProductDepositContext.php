d4c9033ec07615f80fa9dfcee90defdcb2d3ae7d
Gerd Weitenberg <gw@aineko.de>
2021-04-17 21:45:13 +0200

    fix behat setup

--- tests/Behat/Context/Setup/ProductDepositContext.php
+++ tests/Behat/Context/Setup/ProductDepositContext.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Tests\Gewebe\SyliusProductDepositPlugin\Behat\Context\Setup;
 
 use Behat\Behat\Context\Context;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 use Gewebe\SyliusProductDepositPlugin\Entity\ChannelDeposit;
 use Gewebe\SyliusProductDepositPlugin\Entity\ProductVariantInterface;
 use Sylius\Component\Core\Model\ChannelInterface;
